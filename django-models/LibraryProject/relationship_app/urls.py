651515190e3e65a85c1fcb934a84a2cf753bb743
feysel shemsu <feyselshemsu42@gmail.com>
2024-11-17 10:53:56 +0300

    Implement user authentication (login, logout, registration)

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,6 @@
 # relationship_app/urls.py
 from django.urls import path
-from .views import list_books, LibraryDetailView, login_view, logout_view, register  # Correct import for both views
-
-urlpatterns = [
-    path('books/', list_books, name='list_books'),  # Function-based view for listing books
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view for library detail
-]
-
+from . import views  # Import all views at once
 
 urlpatterns = [
     # URL pattern for function-based view (List all books)
@@ -15,7 +9,8 @@ urlpatterns = [
     # URL pattern for class-based view (Library detail)
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-      path('login/', login_view, name='login'),  # User login
-    path('logout/', logout_view, name='logout'),  # User logout
-    path('register/', register, name='register'),  # User registration
+    # User authentication URLs
+    path('login/', views.login_view, name='login'),  # User login
+    path('logout/', views.logout_view, name='logout'),  # User logout
+    path('register/', views.register, name='register'),  # User registration
 ]
