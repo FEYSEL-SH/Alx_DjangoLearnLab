f89525a05b58cc5192b82e6651c3596daa4dfe05
feysel shemsu <feyselshemsu42@gmail.com>
2024-11-17 10:43:38 +0300

    Implemented function-based and class-based views for books and libraries

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,13 @@
 # relationship_app/urls.py
 from django.urls import path
-from . import views
+from django.urls import path
+from .views import list_books, LibraryDetailView  # Correct import for both views
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),  # Function-based view for listing books
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view for library detail
+]
+
 
 urlpatterns = [
     # URL pattern for function-based view (List all books)
