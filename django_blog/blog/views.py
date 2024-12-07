eb1405df338f52a7d83547116a51a05f1a3c6fff
feysel shemsu <feyselshemsu42@gmail.com>
2024-12-07 07:59:02 +0300

    Implement authentication system

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import CustomUserCreationForm
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserChangeForm
+
 
 
 def register(request):
@@ -13,11 +13,26 @@ def register(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Log the user in after registration
-            return redirect('profile')  # Redirect to the profile page
+            login(request, user)
+            return redirect('profile')
     else:
         form = CustomUserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, 'profile.html', {'form': form})
+
+
 
 
 @login_required
