b063ed2b63619aabd8759e5d5b55fca1ae7ad1a8
feysel shemsu <feyselshemsu42@gmail.com>
2024-12-08 11:22:28 +0300

    user authentication

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,30 +1,24 @@
-from django.db import models
+# models.py
 
+from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-
-
-
 class Comment(models.Model):
-    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')  # Link to the Post
-    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Link to the User
-    content = models.TextField()  # Comment text
-    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp for creation
-    updated_at = models.DateTimeField(auto_now=True)  # Timestamp for the last update
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Comment by {self.author} on {self.post}'
+        return f"Comment by {self.author.username} on {self.post.title}"
