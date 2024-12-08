3e009764fc6616412ba267029af2bdf8c19fdaaa
feysel shemsu <feyselshemsu42@gmail.com>
2024-12-08 10:04:42 +0300

    user authentication

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,10 +14,10 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('', PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),  # For creating new posts
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # For updating a post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # For deleting a post
 ]
 
 
