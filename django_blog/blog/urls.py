b4eabe6d5a9254fa982c1c9f9ad6a9b0a259bfcd
feysel shemsu <feyselshemsu42@gmail.com>
2024-12-08 11:25:34 +0300

    user authentication

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -21,10 +21,17 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # For deleting a post
 
 
+
      path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:post_id>/comment/new/', add_comment, name='add-comment'),
     path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+
+
+    path('posts/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
+
+    # URL for updating a comment
+    path('comments/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
 ]
 
 
